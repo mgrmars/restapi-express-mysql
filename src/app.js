8db1e2db6411612fff9e8472f19df0411fe04d19
Miguel Gonzales <88881293+mgrmars@users.noreply.github.com>
2023-01-16 11:46:27 -0500

    CORS fix

--- src/app.js
+++ src/app.js
@@ -10,9 +10,15 @@ app.use(indexRoutes);
 app.use('/api/', employeesRoutes);
 
 app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
   res.status(404).json({
     message: 'Endpoint not found',
   });
+  next();
 });
 
 export default app;
