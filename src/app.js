89dd0097123bc8c0e27e9449609e1375e1aa4061
Miguel Gonzales <88881293+mgrmars@users.noreply.github.com>
2023-01-16 11:56:43 -0500

    cors fix 2

--- src/app.js
+++ src/app.js
@@ -1,24 +1,20 @@
 import express from 'express';
 import employeesRoutes from './routes/employees.routes.js';
 import indexRoutes from './routes/index.routes.js';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(indexRoutes);
 app.use('/api/', employeesRoutes);
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
   res.status(404).json({
     message: 'Endpoint not found',
   });
-  next();
 });
 
 export default app;
